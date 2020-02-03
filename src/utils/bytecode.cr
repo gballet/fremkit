615116cf38aa2c93344fccc3491337d27031d260
Guillaume Ballet <gballet@gmail.com>
2020-02-03 16:55:56 +0100

    Skeleton for the evm interpreter

--- src/utils/bytecode.cr
+++ src/utils/bytecode.cr
@@ -1,3 +1,28 @@
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# For more information, please refer to <http://unlicense.org>
+
 abstract class Assembler
   include Iterator(String)
 
@@ -28,6 +53,20 @@ class EVMAssembler
     "MULMOD"     => {9, 0},
     "EXP"        => {0xa, 0},
     "SIGNEXTEND" => {0xb, 0},
+
+    "LT"     => {0x10, 0},
+    "GT"     => {0x11, 0},
+    "SLT"    => {0x12, 0},
+    "SGT"    => {0x13, 0},
+    "EQ"     => {0x14, 0},
+    "ISZERO" => {0x15, 0},
+    "AND"    => {0x16, 0},
+    "OR"     => {0x17, 0},
+    "XOR"    => {0x18, 0},
+    "NOT"    => {0x19, 0},
+    "BYTE"   => {0x1a, 0},
+
+    "SHA3" => {0x20, 0},
 
     "PUSH1"  => {0x60, 1},
     "PUSH2"  => {0x61, 1},
@@ -119,7 +158,7 @@ class EVMAssembler
       raise Exception.new("Not enough operands to #{tokens[1].upcase} at line #{linenum}")
     end
 
-    if tokens.size > 1 + op[1] && tokens[1 + op[1]] != ";"
+    if tokens.size > 1 + op[1] && tokens[1 + op[1]] =~ /;|\#/
       raise Exception.new("Invalid extra opcodes at line #{linenum}")
     end
 
