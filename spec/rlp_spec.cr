8af92cbd8d7a0e1733a1a9cd67ec1bcd3079ebc8
Guillaume Ballet <gballet@gmail.com>
2020-03-14 15:25:18 +0100

    RLP encoding of complex structures

--- spec/rlp_spec.cr
+++ spec/rlp_spec.cr
@@ -28,52 +28,62 @@ require "./spec_helper.cr"
 include Fremkit::Utils::RLP
 
 describe "prout" do
-    it "should encode 1 as 1 byte" do
-        x = encode(Bytes[1u8])
-        x.size.should eq(1)
-        x[0].should eq(1)
-    end
+  it "should encode 1 as 1 byte" do
+    x = encode(Bytes[1u8])
+    x.size.should eq(1)
+    x[0].should eq(1)
+  end
 
-    it "should encode 129 as 2 bytes" do
-        x = encode(Bytes[129u8])
-        x.size.should eq(2)
-        x.should eq(Bytes[129u8, 129u8])
-    end
+  it "should encode 129 as 2 bytes" do
+    x = encode(Bytes[129u8])
+    x.size.should eq(2)
+    x.should eq(Bytes[129u8, 129u8])
+  end
 
-    it "should encode a 55 bytes array as 56 bytes" do
-        x = encode(Bytes.new(55, 0))
-        x.size.should eq(56)
-        x.should eq(Bytes[183, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-    end
+  it "should encode a 55 bytes array as 56 bytes" do
+    x = encode(Bytes.new(55, 0))
+    x.size.should eq(56)
+    x.should eq(Bytes[183, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+  end
 
-    it "should encode a 56 byte array as 58 bytes" do
-    end
+  it "should encode a 56 byte array as 58 bytes" do
+  end
 
-    it "should be able to decode a byte that it has encoded" do
-        i = Bytes[23u8]
-        r = decode(encode(i))
-        r.should eq(i)
+  it "should be able to decode a byte that it has encoded" do
+    i = Bytes[23u8]
+    r = decode(encode(i))
+    r.should eq(i)
 
-        i = Bytes[157u8]
-        r = decode(encode(i))
-        r.should eq(i)
-    end
+    i = Bytes[157u8]
+    r = decode(encode(i))
+    r.should eq(i)
+  end
 
-    it "should be able to decode a byte array that it has encoded" do
-        i = Bytes[23u8, 35u8, 12u8]
-        r = decode(encode(i))
-        r.should eq(i)
-    end
+  it "should be able to decode a byte array that it has encoded" do
+    i = Bytes[23u8, 35u8, 12u8]
+    r = decode(encode(i))
+    r.should eq(i)
+  end
 
-    it "should be able to decode a long byte array that it has encoded" do
-        i = Random.new.random_bytes(59)
-        r = decode(encode(i))
-        r.should eq(i)
-    end
+  it "should be able to decode a long byte array that it has encoded" do
+    i = Random.new.random_bytes(59)
+    r = decode(encode(i))
+    r.should eq(i)
+  end
 
-    it "should be able to encode and decode a byte array whose length is more than 1 byte wide" do
-        i = Random.new.random_bytes(1024)
-        r = decode(encode(i))
-        r.should eq(i)
-    end
+  it "should be able to encode and decode a byte array whose length is more than 1 byte wide" do
+    i = Random.new.random_bytes(1024)
+    r = decode(encode(i))
+    r.should eq(i)
+  end
+
+  it "should be able to encode a complex structure smaller than 56 bytes" do
+    r = encode [Bytes[1u8, 1u8, 1u8], Bytes[2u8, 2u8, 2u8, 2u8]]
+    r.should eq Bytes[201u8, 131u8, 1u8, 1u8, 1u8, 132u8, 2u8, 2u8, 2u8, 2u8]
+  end
+
+  it "should be able to encode a complex structure bigger than 56 bytes" do
+    r = encode [Bytes[1u8, 1u8, 1u8, 1u8, 1u8, 1u8, 1u8, 1u8, 1u8, 1u8, 1u8, 1u8, 1u8, 1u8, 1u8, 1u8, 1u8, 1u8, 1u8, 1u8, 1u8, 1u8, 1u8, 1u8, 1u8, 1u8, 1u8, 1u8, 1u8, 1u8, 1u8, 1u8], Bytes[2u8, 2u8, 2u8, 2u8, 2u8, 2u8, 2u8, 2u8, 2u8, 2u8, 2u8, 2u8, 2u8, 2u8, 2u8, 2u8, 2u8, 2u8, 2u8, 2u8, 2u8, 2u8, 2u8, 2u8]]
+    r.should eq Bytes[248, 58, 160, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 152, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+  end
 end
--- src/utils/rlp.cr
+++ src/utils/rlp.cr
@@ -25,68 +25,100 @@
 
 # Helper functions for RLP
 module Fremkit::Utils::RLP
-    class DecodeException < Exception
-        def initialize
-            super("RLP decode error")
-        end
+  class DecodeException < Exception
+    def initialize
+      super("RLP decode error")
     end
+  end
 
-    def encode(bytes : Bytes) : Bytes
-        case
-        when bytes.size == 0 then bytes
-        when bytes.size == 1 && bytes[0] < 128u8 then bytes
-        when bytes.size < 56
-            len = bytes.size.to_u8
-            ret = IO::Memory.new(len+1)
-            ret.write_byte(128u8+len)
-            ret.write(bytes)
-            ret.to_slice
-        else
-            len = bytes.size        # Length of payload in bytes
-            lenlen = case len       # Number of bytes to encode the length above
-            when 0..255 then 1
-            when 256..65535 then 2
-            when 65536..16777215 then 3
-            else 4
-            end
-
-            ret = IO::Memory.new(len+lenlen+1)
-            # Array size is at most I32 in Crystal
-            
-            
-            ret.write_byte(183u8+lenlen)
-            ret.seek(lenlen+1)
-            ret.write(bytes)
-
-            buf = uninitialized UInt8[4]
-            IO::ByteFormat::BigEndian.encode(len, buf.to_slice)
-            buf.to_slice[-lenlen..-1].copy_to(ret.to_slice[1..len])
-
-            ret.to_slice
-        end
+  # General version: serialize all items one by one. This won't support
+  # payloads more than 64K in total.
+  def encode(items : Array) : Bytes
+    # For the initial capacity, assume that most items will be a multiple
+    # of 32 so including the header and the encoding, assume it is going
+    # to be 3 + 33 * items.size as a rule of thumb. And then round it up
+    # to a page size.
+    encoding = IO::Memory.new(2 + 33*items.size)
+    encoding.write_byte(0) # placeholders for the header, 64K-3 max
+    encoding.write_byte(0)
+    encoding.write_byte(0)
+    items.each do |item|
+      encoding.write encode(item)
     end
-
-    def decode(bytes : Bytes) : Bytes
-        case bytes[0]
-        when 0..127
-            raise DecodeException.new if bytes.size != 1
-            bytes
-        when 128..182
-            length = bytes[0] - 128
-            raise DecodeException.new if length >= 56 || (bytes.size - length != 1)
-            ret = IO::Memory.new(length)
-            ret.write(bytes[1..])
-            ret.to_slice
-        else
-            length_length = bytes[0] - 183
-            raise DecodeException.new if (bytes.size - length_length <= 1)
-            buf = Bytes.new(4, 0)
-            buf[-length_length.to_i32..].copy_from(bytes[1,length_length])
-            length = IO::ByteFormat::BigEndian.decode(Int32, buf)
-            raise DecodeException.new if (bytes.size - length_length - 1 - length) < 0
-            buffer = Bytes.new(length, 0)
-            bytes[1+length_length..].copy_to(buffer)
-            buffer
-        end
+    payload_size = encoding.pos - 3
+    ret = encoding.to_slice
+    if payload_size < 56
+      ret[2] = 192u8 + payload_size.to_u8
+      return ret[2..]
+    elsif payload_size < 256
+      ret[1] = 248
+      ret[2] = payload_size.to_u8
+      return ret[1..]
+    elsif payload_size < 65533
+      ret[0] = 249
+      IO::ByteFormat::BigEndian.encode(payload_size.to_u16, ret[1..2])
+      return ret
+    else
+      raise "RLP payload is too big"
     end
+  end
+
+  # Specialize template for bytes
+  def encode(bytes : Bytes) : Bytes
+    case
+    when bytes.size == 0                     then bytes
+    when bytes.size == 1 && bytes[0] < 128u8 then bytes
+    when bytes.size < 56
+      len = bytes.size.to_u8
+      ret = IO::Memory.new(len + 1)
+      ret.write_byte(128u8 + len)
+      ret.write(bytes)
+      ret.to_slice
+    else
+      len = bytes.size  # Length of payload in bytes
+      lenlen = case len # Number of bytes to encode the length above
+               when 0..255          then 1
+               when 256..65535      then 2
+               when 65536..16777215 then 3
+               else                      4
+               end
+
+      ret = IO::Memory.new(len + lenlen + 1)
+      # Array size is at most I32 in Crystal
+
+      ret.write_byte(183u8 + lenlen)
+      ret.seek(lenlen + 1)
+      ret.write(bytes)
+
+      buf = uninitialized UInt8[4]
+      IO::ByteFormat::BigEndian.encode(len, buf.to_slice)
+      buf.to_slice[-lenlen..-1].copy_to(ret.to_slice[1..len])
+
+      ret.to_slice
+    end
+  end
+
+  def decode(bytes : Bytes) : Bytes
+    case bytes[0]
+    when 0..127
+      raise DecodeException.new if bytes.size != 1
+      bytes
+    when 128..182
+      length = bytes[0] - 128
+      raise DecodeException.new if length >= 56 || (bytes.size - length != 1)
+      ret = IO::Memory.new(length)
+      ret.write(bytes[1..])
+      ret.to_slice
+    else
+      length_length = bytes[0] - 183
+      raise DecodeException.new if (bytes.size - length_length <= 1)
+      buf = Bytes.new(4, 0)
+      buf[-length_length.to_i32..].copy_from(bytes[1, length_length])
+      length = IO::ByteFormat::BigEndian.decode(Int32, buf)
+      raise DecodeException.new if (bytes.size - length_length - 1 - length) < 0
+      buffer = Bytes.new(length, 0)
+      bytes[1 + length_length..].copy_to(buffer)
+      buffer
+    end
+  end
 end
