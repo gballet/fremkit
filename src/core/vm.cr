0a45495ce54c0baa8f33afc0f9778bcfb64bb846
Guillaume Ballet <gballet@gmail.com>
2020-05-03 18:04:59 +0200

    evm: Implement the EVM as inheriting from VM

--- src/core/vm.cr
+++ src/core/vm.cr
@@ -25,6 +25,9 @@
 
 require "big"
 
+require "./state"
+require "../common/address"
+
 class ExecutionContext
   property gas : UInt64 = 0
   getter address : BigInt = BigInt.new(0)
@@ -36,4 +39,205 @@ class ExecutionContext
   getter code : Bytes = Bytes.empty
   getter gasprice : UInt64 = 0
   property retdata : Bytes = Bytes.empty
+end
+
+abstract class Registers
+  abstract def [](name : String) : Int
+end
+
+abstract class VM
+  abstract def step
+  abstract def registers : Registers
+  abstract def memory : Bytes
+  abstract def run
+end
+
+class EVM < VM
+  class EVMRegisters < Registers
+    def initialize(pc : UInt64)
+      @pc = pc
+    end
+
+    def [](name : String) : Int
+      raise "Invalid register #{name}" if name != "pc"
+      @pc
+    end
+  end
+
+  def initialize(@code : Bytes, @mem : Bytes, @context : ExecutionContext, @state : Fremkit::Core::State(Fremkit::Core::Address, BigInt))
+    @pc = 0
+    @done = false
+    @stack = Array(BigInt).new
+  end
+
+  def step
+    if @pc < @code.size
+      instr = @code[@pc]
+
+      case instr
+      when 0 # STOP
+        done = true
+      when 1 # ADD
+        a = @stack.pop
+        b = @stack.pop
+        @stack.push (a + b)
+      when 2 # MUL
+        a = @stack.pop
+        b = @stack.pop
+        @stack.push (a * b)
+      when 0x10 # LT
+        a = @stack.pop
+        b = @stack.pop
+        @stack.push BigInt.new(a < b ? 1 : 0)
+      when 0x11 # GT
+        a = @stack.pop
+        b = @stack.pop
+        @stack.push BigInt.new(a > b ? 1 : 0)
+      when 0x14 # EQ
+        a = @stack.pop
+        b = @stack.pop
+        @stack.push BigInt.new(a == b ? 1 : 0)
+      when 0x15 # ISZERO
+        a = @stack.pop
+        @stack.push BigInt.new(a == 0 ? 1 : 0)
+      when 0x16 # AND
+        a = @stack.pop
+        b = @stack.pop
+        @stack.push (a & b)
+      when 0x17 # OR
+        a = @stack.pop
+        b = @stack.pop
+        @stack.push (a | b)
+      when 0x18 # XOR
+        a = @stack.pop
+        b = @stack.pop
+        @stack.push (a ^ b)
+      when 0x19 # NOT
+        n = @stack.pop
+        o = BigInt.new(0)
+        256.times do |bit|
+          o |= (1 << bit) if n.bit(bit) == 0
+        end
+      when 0x1a # BYTE
+        byte_num = @stack.pop
+        src = @stack.pop
+        @stack.push ((src >> (8*byte_num)) & 0xFF)
+      when 0x30 # ADDRESS
+        @stack.push @context.address
+      when 0x31 # BALANCE
+        @stack.push @context.state[@context.address]
+      when 0x32 # ORIGIN
+        @stack.push @context.origin
+      when 0x33 # CALLER
+        @stack.push @context.caller
+      when 0x34 # CALLVALUE
+        @stack.push @context.callvalue
+      when 0x35 # CALLDATALOAD
+        off = @stack.pop
+        result = BigInt.new(0)
+        32.times do |i|
+          x = if off + i >= @context.calldata.size
+                0
+              else
+                @context.calldata[off + i]
+              end
+          result = (result << 8) | x
+        end
+        @stack.push result
+      when 0x36 # CALLDATASIZE
+        @stack.push BigInt.new @context.calldata.size
+      when 0x37 # CALLDATACOPY
+        addr = @stack.pop
+        input_off = @stack.pop
+        len = @stack.pop
+        len.times do |i|
+          if @context.calldata.size > input_off + i
+            @mem[addr + i] = @context.calldata[input_off + i]
+            # TODO check memory extension
+          else
+            # Terminate if trying to read beyond the input data size
+            done = true
+          end
+        end
+      when 0x38 # CODESIZE
+        @stack.push BigInt.new @context.code.size
+      when 0x39 # CODECOPY
+        addr = @stack.pop
+        code_off = @stack.pop
+        len = @stack.pop
+        len.times do |i|
+          if @context.calldata.size > code_off + i
+            @mem[addr + i] = @context.calldata[code_off + i]
+            # TODO check memory extension
+          else
+            # Terminate if trying to read beyond the input data size
+            done = true
+          end
+        end
+      when 0x3a # GASPRICE
+        @stack.push BigInt.new @context.gasprice
+      when 0x50 # POP
+        @stack.pop
+      when 0x54 # SLOAD
+        addr = @stack.pop
+        @stack.push (@state[addr] || BigInt.new)
+      when 0x55 # SSTORE
+        addr = @stack.pop
+        word = @stack.pop
+        state[addr] = word
+      when 0x56 # JUMP
+        addr = @stack.pop
+        pc = addr.to_u16 - 1
+        # when 0x58 # JUMPI
+        # addr = @stack.pop
+        # cond = @stack.pop
+        # unless cond.zero?
+        # pc = addr.to_u16 - 1
+        # end
+      when 0x58 # PC
+        @stack.push BigInt.new(pc)
+      when 0x59 # MSIZE
+        @stack.push BigInt.new(mem.size)
+      when 0x5a # GAS
+        @stack.push BigInt.new @context.gas
+      when 0x5b # JUMPDEST
+        # Does nothing
+      when 0x60..0x7f # PUSHn
+        datasize : UInt8 = instr - 0x60
+        data = BigInt.new
+        datasize.times do |i|
+          data += bytecode[pc + i] << (1*8)
+        end
+        @stack.push data
+      when 0x80..0x8f # DUPn
+        depth : UInt8 = instr - 0x60
+        @stack.push @stack[@stack.size - 1 - depth]
+      when 0x90..0x9f # SWAPn
+        sdepth : UInt8 = instr - 0x60
+        tmp = @stack[@stack.size - 1 - sdepth]
+        @stack[@stack.size - 1 - sdepth] = @stack[@stack.size - 1]
+        @stack[@stack.size - 1] = tmp
+      else
+        raise Exception.new "Unsupported instruction: #{instr}"
+      end
+    else
+      raise Exception.new "Invalid program counter: #{pc} < #{bytecode.size} = bytecode size"
+    end
+
+    pc += 1
+  end
+
+  def registers : Registers
+    EVMRegisters.new(@pc)
+  end
+
+  def memory : Bytes
+    @mem
+  end
+
+  def run
+    while !@done
+      step
+    end
+  end
 end
