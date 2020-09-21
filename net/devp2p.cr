3230e03bad95cbd5980dc1cdfbad1bfb2b04025f
Guillaume Ballet <gballet@gmail.com>
2020-09-21 17:41:14 +0200

    devp2p: skeleton for standard devp2p messages

--- net/devp2p.cr
+++ net/devp2p.cr
@@ -2,4 +2,47 @@ module DevP2P
   abstract class Message
     abstract def payload : Bytes
   end
+
+  module Messages
+    class Hello < Message
+      def payload : Bytes
+      end
+    end
+
+    class Disconnect < Message
+      enum Reason
+        DisconnectRequested
+        TCPSubSystemError
+        BreachOfProtocol
+        UselessPeer
+        TooManyPeers
+        AlreadyConnected
+        IncompatibleP2PProtocolVersion
+        NullNodeIdentity
+        ClientQuit
+        UnexpectedIdentityInHandshake
+        ConnectedToSelf
+        PingTimeout
+        Other
+      end
+
+      @reason : Reason = Reason::Other
+
+      def payload : Bytes
+        self.to_rlp
+      end
+    end
+
+    class Ping < Message
+      def payload : Bytes
+        self.to_rlp
+      end
+    end
+
+    class Pong < Message
+      def payload : Bytes
+        self.to_rlp
+      end
+    end
+  end
 end
