3f1cfe462335cf55d6c3868750844da1be1adfd8
Guillaume Ballet <gballet@gmail.com>
2020-10-14 21:19:47 +0200

    trie: unit tests for the two previous cases

--- spec/trie_spec.cr
+++ spec/trie_spec.cr
@@ -36,6 +36,21 @@ describe "hexary trie:" do
       key = Bytes[0, 1, 2, 3]
       Trie::EmptyNode.new.hex_prefix(key, true).should eq Bytes[32, 1, 35]
     end
+  it "should be able to insert a (key,value) pair into the empty root" do
+    trie = Trie.new
+    trie.root_hash.should eq Trie::EmptyRoot
+
+    trie.insert Bytes[0, 1, 2, 3], Bytes[4, 5, 6, 7]
+    trie.get(Bytes[0, 1, 2, 3]).should eq Bytes[4, 5, 6, 7]
+    trie.root_hash.should eq Bytes[210, 173, 155, 246, 191, 47, 142, 155, 159, 177, 26, 9, 92, 220, 46, 254, 182, 70, 215, 123, 170, 170, 69, 47, 203, 11, 122, 110, 126, 103, 233, 158]
   end
 
+  it "should be able to insert a (key,value) pair into a leaf root" do
+    trie = Trie.new
+    trie.root_hash.should eq Trie::EmptyRoot
+
+    trie.insert Bytes[0, 1, 2, 3], Bytes[4, 5, 6, 7]
+    trie.insert Bytes[0, 1, 3, 3], Bytes[8, 9, 10, 11]
+    trie.root_hash.should eq Bytes[186, 193, 14, 228, 135, 231, 231, 163, 129, 218, 34, 128, 99, 36, 34, 250, 51, 145, 99, 121, 10, 223, 66, 220, 84, 254, 118, 75, 125, 176, 199, 170]
+  end
 end
